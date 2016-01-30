b30f054b3205f37e9b1ae19814e8caf14db2afb2
Joey Arnold <jandres@ualberta.ca>
2016-01-29 20:13:51 -0700

    Updated to v2.0.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "jandres:snapjs",
     summary: "A Library for creating beautiful mobile shelfs in Javascript (Facebook and Path style side menus)",
-    version: "1.9.7",
+    version: "2.0.1",
     git: "https://github.com/JoeyAndres/Snap.js.git"
 });
 
